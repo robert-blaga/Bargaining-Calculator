51a325e67a72bc574c017228668aaec3cff63f2f
robert-blaga <98452793+robert-blaga@users.noreply.github.com>
2022-02-23 14:26:25 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-# This is a simple application to determine the bargaining positions for negotiating prices
-# The rule states that:
-# You first need to establish the target price, then
+
+# This is a simple application to determine the bargaining positions for negotiating prices (Ackerman technique)
+
 # as a Buyer, you propose offers that are at 65 - 85 - 95 - 100% of the target price
 # as a Seller, you propose  offers that are at 135 - 115 - 105 - 100% of the offered price
 
 
-# Determine the starting position (buyer or seller)
-
+# Determine the role you have (buyer or seller)
 while True:
     try:
-        position_type = str(input("Are you a buyer or a seller? Type B or S: "))
+        role_type = str(input("Are you a buyer or a seller? Type B or S: "))
     except ValueError:
         continue
-    if position_type != "B" and position_type != "S" and position_type != "b" and position_type != "s":
-        print(position_type + " is not a known position")
+    if role_type != "B" and role_type != "S" and role_type != "b" and role_type != "s":
+        print(role_type + " is not a known position")
         continue
     else:
         break
 
+# Determine the target price
 target_price = float(input("What is the target price: "))
 
-
+# The bargaining % for each position
 offer_steps_buyer = [target_price * 0.65, target_price * 0.85, target_price * 0.95, target_price]
 offer_steps_seller = [target_price * 1.35, target_price * 1.15, target_price * 1.05, target_price]
 
@@ -36,7 +36,7 @@ def offers_seller():
     print("Third offer: " + str(int(offer_steps_seller[2])))
     print("Fourth offer: " + str(int(offer_steps_seller[3])))
 
-if position_type == "B":
+if role_type == "B":
     offers_buyer()
 else:
     offers_seller()
